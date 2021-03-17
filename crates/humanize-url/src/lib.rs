a4c98088c42780a8e15eb625a166d286087ead49
SirWindfield <SirWindfield@users.noreply.github.com>
2021-03-17 12:10:14 +0100

    chore(humanize-url): add implementation

--- crates/humanize-url/src/lib.rs
+++ crates/humanize-url/src/lib.rs
@@ -1,7 +1,45 @@
+pub use unrestrictive_url::ParseError;
+use unrestrictive_url::{UnrestrictiveUrl, Url};
+
+pub fn humanize_url(url: &str) -> Result<String, ParseError> {
+    let url = Url::parse(url)?;
+    let mut url = UnrestrictiveUrl::from(&url);
+
+    // Remove protocol.
+    url.scheme = None;
+    // Remove authentication.
+    url.username = None;
+    url.password = None;
+
+    // Remove trailing slashes.
+    let url = url.to_string();
+    let mut chars = url.chars();
+    if chars.next_back() == Some('/') {
+        Ok(chars.collect())
+    } else {
+        Ok(url)
+    }
+}
+
 #[cfg(test)]
 mod tests {
+    use super::humanize_url;
+
     #[test]
-    fn it_works() {
-        assert_eq!(2 + 2, 4);
+    fn removes_scheme() {
+        let url = humanize_url("https://github.com/SirWindfield").unwrap();
+        assert_eq!("github.com/SirWindfield", url);
+    }
+
+    #[test]
+    fn removes_trailing_slash() {
+        let url = humanize_url("https://github.com/SirWindfield/").unwrap();
+        assert_eq!("github.com/SirWindfield", url);
+    }
+
+    #[test]
+    fn removes_authentication() {
+        let url = humanize_url("https://user:pw@github.com/SirWindfield").unwrap();
+        assert_eq!("github.com/SirWindfield", url);
     }
 }
